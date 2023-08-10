924db5aa9cc8245a69fefc1c90933dacf2a188c9
Vikram <vikramvikky2003@gmail.com>
2023-08-11 01:28:55 +0530

    Build Sample Context in Django.

--- Restaurent_Menu/views.py
+++ Restaurent_Menu/views.py
@@ -6,6 +6,12 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
+    def get_context_data(self):
+        context = {"meals": ["Pizza", "Pasta"],
+                   "ingradients": ["Things"]}
+        return context
+
+
 class MenuItemDetail(generic.DetailView):
     model = Item
     template_name = "menu_item_detail.html"
