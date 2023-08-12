c4d8625ff5edf426ac9119d9ebe15facc64630ba
Vikram <vikramvikky2003@gmail.com>
2023-08-12 23:29:46 +0530

    Adding dynamic links and bootstrap.

--- Restaurent_Menu/urls.py
+++ Restaurent_Menu/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MenuList.as_view(), name='home') # Home is having empty string,,, as_view() is not used in func based view
+    path('', views.MenuList.as_view(), name='home'),  # Home is having empty string,,, as_view() is not used in func based view
+    path('item/<int:pk>/', views.MenuItemDetail.as_view(), name="menu_item")
 ]
